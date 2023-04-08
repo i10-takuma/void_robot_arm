e891b5bff9939f338adeceb6a3fe17a1633b061e
takuma <ito.takuma.9tfm@gmail.com>
2023-04-08 13:22:00 +0900

    ステッピングモーター制御追加

--- scripts/robot_arm_move.py
+++ scripts/robot_arm_move.py
@@ -3,20 +3,51 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import JointState
 from Adafruit_PCA9685 import PCA9685
+#import RPi.GPIO as GPIO
+import wiringpi
+import time
 import math
 
-servo_min = 100
-servo_max = 510
+# サーボモーターの設定
+servo_min = 100  # 最小パルス幅 (ms)
+servo_max = 510  # 最大パルス幅 (ms)
 
+# PCA9685のI2Cアドレス
 pwm = PCA9685()
+# PWM周波数
 pwm.set_pwm_freq(50)
 
+# Step motor
+before_angle = 0
+step_pin = 20
+dir_pin = 21
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(step_pin, 1)
+wiringpi.pinMode(dir_pin, 1)
+
+# 目標角度の受け取り用コールバック関数
 def joint_states_callback(msg):
     joint_angles = msg.position
     print(msg)
+    
+    # ステッピングモータ
+    step = int((1000 * math.degrees(joint_angles[0]) / 360) - before_angle)
+    if step > 0:
+        wiringpi.digitalWrite(dir_pin, 1)
+    else:
+        wiringpi.digitalWrite(dir_pin, 0)
+    step = abs(step)
+    for i in range(step):
+        wiringpi.digitalWrite(step_pin, 1)
+        time.sleep(0.001)
+        wiringpi.digitalWrite-(step_pin, 0)
+        time.sleep(0.001)
+    before_angle = math.degrees(joint_angles[0])
+    
+    # サーボモータ
     pulse0 = int((servo_max - servo_min)*(math.degrees(joint_angles[1]) + 90) / 180 + servo_min)
     pulse1 = int((servo_max - servo_min)*(180 - math.degrees(joint_angles[1]) - 90) / 180 + servo_min)
-    pulse2 = int((servo_max - servo_min)*(math.degrees(joint_angles[2]) + 100) / 180 + servo_min)
+    pulse2 = int((servo_max - servo_min)*(math.degrees(joint_angles[2]) + 100) / 180 + servo_min) 
     pulse3 = int((servo_max - servo_min)*(180 - math.degrees(joint_angles[2]) - 80) / 180 + servo_min)
     pulse4 = int((servo_max - servo_min)*(math.degrees(joint_angles[3]) + 90) / 180 + servo_min)
     pulse5 = int((servo_max - servo_min)*(math.degrees(joint_angles[4]) + 90) / 180 + servo_min)
@@ -28,6 +59,7 @@ def joint_states_callback(msg):
     pwm.set_pwm(4, 0, pulse4)
     pwm.set_pwm(5, 0, pulse5)
     pwm.set_pwm(7, 0, pulse6)
+
 
 def listener():
     rospy.init_node('robot_arm_move', anonymous=True)
